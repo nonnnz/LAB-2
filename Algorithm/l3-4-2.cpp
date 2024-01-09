b812ed9fe171ccac5bff4af08884b6c487b1efe0
Ratchanon <ratchanonmcnon2015@gmail.com>
2024-01-10 00:02:35 +0700

    Refactor maxSubArraySum function and remove commented code

--- Algorithm/l3-4-2.cpp
+++ Algorithm/l3-4-2.cpp
@@ -15,26 +15,27 @@ int maxSubArraySum(int a[], int l, int r) {
     int max_right_sum = MIN;
     for(int i = mid-1, sum = 0; i >= l; i--) {
         sum += a[i];
-        cout<<i<<" "<<sum<<endl;
         max_left_sum = max(max_left_sum, sum);
     }
     for(int i = mid+1, sum = 0; i <= r; i++) {
         sum += a[i];
         max_right_sum = max(max_right_sum, sum);
     }
-
-    int max_mid_sum = max_left_sum + max_right_sum + a[mid];
+    // cout<<max_left_sum<<" "<<max_right_sum<<endl;
+    int max_mid_sum = max(max_left_sum + max_right_sum + a[mid],max(max_left_sum + a[mid], max_right_sum + a[mid]));
+    // cout<<max_left<<" "<<max_right<<" "<<max_mid_sum<<endl;
     return max(max(max_left, max_right), max_mid_sum);
 }
 
 int main() {
-    int a[] = {2, -4, 1, 9, -6, 7, -3};
+    // int a[] = {2, -4, 1, 9, -6, 7, -3};
     // int a[] = {-1, -3, -4, -5, -9};
-    int n = sizeof(a) / sizeof(a[0]);
-    // int n;
-    // cin>>n;
-    // int a[n];
-    // for(int i = 0; i < n; i++) cin>>a[i];
+    // int a[] = {-1, -1, 9, 9, -1, -1};
+    // int n = sizeof(a) / sizeof(a[0]);
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i = 0; i < n; i++) cin>>a[i];
     int max_sum = maxSubArraySum(a, 0, n-1);
     cout<<max_sum;
     return 0;
