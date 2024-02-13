3fbff8ae73179e62535fb5c33bd2a063099869ea
Ratchanon <ratchanonmcnon2015@gmail.com>
2024-02-14 01:20:43 +0700

    Update Algo MID

---
+++ Algorithm/AFTER-MID/p2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int a,b,n;
+	cin>>a>>b;
+	string c;
+	cin>>c;
+	cin>>n;
+	
+	cout<<a<<" "<<b<<endl;
+	cout<<c<<endl;
+	cout<<n<<endl;
+}
